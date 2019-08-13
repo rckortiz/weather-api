ad8f9a7122f2d4caf988a9854ef0e0c87b5f05c2
Rick Ortiz <rick@Ricks-MacBook-Pro.local>
2019-08-13 18:48:50 -0400

    site functional

--- public/main.js
+++ public/main.js
@@ -1,7 +1,26 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+let textInput = document.querySelector('.location-field')
+
+const search = () => {
+  let userInput = textInput.value
+  const cityApi = 'https://api.openweathermap.org/data/2.5/weather'
+  const apiKey = '4012072fbc944a45f6dbbb0ac17d8cb0'
+  const newUrl =
+    cityApi + '?' + 'q=' + userInput + '&units=imperial' + '&appid=' + apiKey
+  console.log(newUrl)
+  return newUrl
 }
 
-document.addEventListener('DOMContentLoaded', main)
+const getWeather = () => {
+  fetch(search())
+    .then(response => {
+      return response.json()
+    })
+    .then(textInput => {
+      console.log(textInput)
+      document.querySelector('.forecast').textContent =
+        textInput.weather[0].main
+      document.querySelector('.temp').textContent = textInput.main.temp
+    })
+}
+
+document.querySelector('.search-button').addEventListener('click', getWeather)
